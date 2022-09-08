3b74263c84d7bb903f97d8496a357305093b5f34
ingeomar2021 <ingeomar2001@gmail.com>
2022-09-07 20:57:12 -0500

    commit 2

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -5,6 +5,8 @@ public class GeneradorVertebrados {
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Perro", "Pez"); 
         miVertebrados[3] = new Vertebrado("Perro2", "Pez2"); 
+        miVertebrados[4] = new Vertebrado("Perro2", "Pez2"); 
+        miVertebrados[5] = new Vertebrado("Perro2", "Pez2"); 
         
         return miVertebrados;
     }
